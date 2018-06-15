eb579293e9bbc70c555aacf508de1727aeb1d938
Mihail Krivickiy <mkrivickiy@gmail.com>
2018-06-15 10:31:01 +0300

    fix garbage

--- main.go
+++ main.go
@@ -3,20 +3,11 @@ package main
 import (
 	"crm-management/systems/amocrm"
 	"fmt"
-	"net/http"
 )
-
-func HomeHandler(w http.ResponseWriter, req *http.Request) {}
 
 func main() {
 
 	amocrm := amocrm.Amocrm{}
 	result := amocrm.Authentificate("mkrivickiy@gmail.com", "470710cae5dedc3a48713b0f07145125")
 	fmt.Println(result)
-	// router := mux.NewRouter()
-	// router.HandleFunc("/", HomeHandler)
-	// http.Handle("/", router)
-
-	// log.Fatal(http.ListenAndServe("prolx.loc:1234", nil))
-
 }
